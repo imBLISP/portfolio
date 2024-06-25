4ea9cf70239f2c53ba081ba7393b5b27fa7f7248
blisp <vineetpatil@outlook.com>
2024-06-25 12:35:23 +0530

    Nav tabs cleanup

--- src/ui/layout/nav-tabs.tsx
+++ src/ui/layout/nav-tabs.tsx
@@ -5,31 +5,26 @@ import { useActiveSectionContext } from "../Sections/provider";
 import { NavItems } from "@/lib/data/data";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useMediaQuery } from "../hooks/use-media-query";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import useScrollBehavior from "../hooks/use-scroll-behavior";
+import { usePathname, useRouter } from "next/navigation";
 import {
   Drawer,
-  DrawerClose,
   DrawerContent,
-  DrawerDescription,
   DrawerFooter,
   DrawerHeader,
-  DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function NavTabs() {
   const router = useRouter();
   const pathName = usePathname();
-  // const searchParams = useSearchParams();
   const [open, setOpen] = useState(false);
-  const { device, isDesktop } = useMediaQuery();
   useScrollBehavior(open);
-
+  const { device, isDesktop } = useMediaQuery();
   const { activeSection, setActiveSection, setActiveSectionChangeTime } =
     useActiveSectionContext();
 
@@ -46,34 +41,24 @@ export default function NavTabs() {
     );
   };
 
-  // useEffect(() => {
-  //   console.log("route changed")
-  //   setTimeout(() => {
+  const handleNavItemClick = (name: SectionName) => {
+    setActiveSection(name);
+    setActiveSectionChangeTime(new Date());
+  };
 
-  //   if (window.location.hash) {
-  //     scrollToId(window.location.hash, 0);
-  //   }
-  //   }, 100)
-  // }, [pathName]);
-
-  const handleClick = (
+  const handleDrawerItemClick = (
     name: SectionName,
-    date: Date,
-    id: SectionId | null = null,
+    id: SectionId,
     timeout: number = 0
   ) => {
     setActiveSection(name);
-    setActiveSectionChangeTime(date);
-    if (id) {
-      setOpen(false);
-      if (pathName == '/') {
-        scrollToId(id, timeout)
-      }
-      else {
-        setTimeout(() => router.push(`/${id}`), timeout)
-      }
-      // router.push(`/${id}`)
-      // scrollToId(id, timeout)
+    setActiveSectionChangeTime(new Date());
+
+    setOpen(false);
+    if (pathName == "/") {
+      scrollToId(id, timeout);
+    } else {
+      setTimeout(() => router.push(`/${id}`), timeout);
     }
   };
 
@@ -91,12 +76,11 @@ export default function NavTabs() {
         {NavItems.map(({ name, id }) => (
           <Link
             href={`/${id}`}
-            onClick={() => handleClick(name, new Date())}
+            onClick={() => handleNavItemClick(name)}
             className={cn(
-              " text-foreground/70 group inline-flex h-10 w-max items-center justify-center pr-8 py-2 text-base font-medium transition-colors hover:text-foreground/100",
+              "pr-8 py-2 h-10 w-max font-medium text-black text-foreground/70 text-base transition-colors hover:text-foreground/100 group inline-flex items-center justify-center",
               { "text-foreground/100": name == activeSection }
             )}
-            // scroll={false}
             key={name}
           >
             {name}
@@ -111,7 +95,7 @@ export default function NavTabs() {
       <Drawer open={open} onOpenChange={setOpen}>
         <DrawerTrigger
           className={cn(
-            "text-foreground/70 group inline-flex h-10 w-max items-center justify-center pr-8 py-2 text-base font-medium transition-colors hover:text-foreground/100"
+            "pr-8 py-2 h-10 w-max font-medium text-black text-foreground/70 text-base transition-colors hover:text-foreground/100 group inline-flex items-center justify-center"
           )}
         >
           Drawer
@@ -120,10 +104,8 @@ export default function NavTabs() {
           <DrawerHeader>
             {NavItems.map(({ name, id }) => (
               <Button
-                // href={`/${id}`}
                 key={name}
-                onClick={() => handleClick(name, new Date(), id, 600)}
-                // scroll={false}
+                onClick={() => handleDrawerItemClick(name, id, 600)}
                 variant="ghost"
               >
                 {name}
